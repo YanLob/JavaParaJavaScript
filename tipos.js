e2d387de019d5b987fcf5e210ddece421696d61b
YanLob <yanlobate@gmail.com>
2023-07-07 11:43:06 -0300

    a

---
+++ tipos.js
@@ -0,0 +1,12 @@
+const cliente = 
+{
+    nome: "lucas",
+    email: "lucas.ferreira@gmail.com",
+    telefone: ["11 12323200", "11 32131533"]    
+}
+
+const chave = ["nome", "email", "telefone"]
+
+chave.forEach((chave) => {
+    console.log(`O ${chave} do cliente é ${cliente[chave]}`)
+})
