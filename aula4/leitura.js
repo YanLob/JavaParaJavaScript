e884f576bf8322a5f387b2e083509a8fe3b5e3f6
YanLob <yanlobate@gmail.com>
2023-07-12 20:05:18 -0300

    Usando JSON

---
+++ aula4/leitura.js
@@ -0,0 +1,10 @@
+const dados = require("./cliente.json")
+console.log(dados)
+
+const clienteEmString = JSON.stringify(dados)
+
+console.log(clienteEmString)
+
+const clienteEmObjeto = JSON.parse(clienteEmString)
+
+console.log(clienteEmObjeto)
