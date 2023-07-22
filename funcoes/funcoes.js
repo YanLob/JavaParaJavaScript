7632fd589242870c300377130580ab7fc9095975
YanLob <yanlobate@gmail.com>
2023-07-22 16:01:43 -0300

    1

---
+++ funcoes/funcoes.js
@@ -0,0 +1,15 @@
+//console.log("---Tabuada---")
+
+//function tabuada (num1) {
+//    for (var i = 1; i <= 10; i++){
+//        console.log(`${num1} X ${i} = ${num1*i}`)
+//    }
+//}
+
+//tabuada(10)
+
+function soma (num1, num2) { return num1 + num2 }
+
+function multiplicacao (num1, num2) { return num1 * num2 }
+
+console.log(multiplicacao(soma(5, 4), soma(3, 3)))
