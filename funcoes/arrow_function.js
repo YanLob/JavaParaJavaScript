650830ce07a64498749f62433dc9e78d38808428
YanLob <yanlobate@gmail.com>
2023-07-22 16:36:51 -0300

    4

--- funcoes/arrow_function.js
+++ funcoes/arrow_function.js
@@ -1,3 +1,11 @@
 const soma = (num1, num2) => num1 + num2;
 
 console.log(soma(5, 5));
+
+const tabuada = num =>{
+    for (let i = 1; i <= 10; i++){
+        console.log(`${num} X ${i} = ${num*i}`)
+    }
+}
+
+tabuada(10)
