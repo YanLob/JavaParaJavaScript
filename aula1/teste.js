8b8a18c42f990837b2ab5c68b5532db22e6310e9
YanLob <yanlobate@gmail.com>
2023-08-12 19:06:24 -0300

    exercicio 3 an0

--- aula1/teste.js
+++ aula1/teste.js
@@ -1,7 +1,54 @@
-const tabuada = function(valor) {
-    for (var i = 1; i <= 10; i++) {
-        console.log(`${i} X ${valor} = ${i*valor}`);
+const Pessoa = {
+    nome: "",
+    quantidadeFilhos: 0,
+    salario: 0,
+    taxa: 0,
+    calcular(){
+
+        if (this.salario < 2500) 
+        {
+            console.log("Seu salário não sofreu alterações.");
+        }
+
+        else if (this.salario > 2500 && this.salario < 5000)
+        {
+
+            if (this.quantidadeFilhos === 0) {
+                this.taxa = this.salario * 0.20;
+            }
+
+            else if (this.quantidadeFilhos === 1) {
+                this.taxa = this.salario * 0.15;
+            }
+
+            else if (this.quantidadeFilhos >= 2) {
+                this.taxa = this.salario * 0.10;
+            }
+
+        }
+
+        else if(this.salario >= 5000) 
+        {
+
+            if (this.quantidadeFilhos === 0){
+                this.taxa = this.salario * 0.30;
+            }
+
+            else if (this.quantidadeFilhos === 1){
+                this.taxa = this.salario * 0.28;
+            }
+
+            else if (this.quantidadeFilhos >= 2){
+                this.taxa = this.salario * 0.22;
+            }
+
+        }
+        console.log(`${this.nome} seu salário com o desconto do imposto ficou R$${this.salario -= this.taxa}.`);
     }
 }
 
-tabuada(9)
+Pessoa.nome = "Renato Cariane"
+Pessoa.quantidadeFilhos = 3
+Pessoa.salario = 18000
+
+Pessoa.calcular()
--- funcoes/arrow_function.js
+++ funcoes/arrow_function.js
@@ -1,7 +1,7 @@
 const soma = (num1, num2) => num1 + num2;
-
+                    
 console.log(soma(5, 5));
-
+            
 const tabuada = num =>{
     for (let i = 1; i <= 10; i++){
         console.log(`${num} X ${i} = ${num*i}`)
