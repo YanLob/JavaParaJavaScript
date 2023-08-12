f6748e98f7b934a07624132d83d55c7f2efc6c1b
YanLob <yanlobate@gmail.com>
2023-08-12 19:16:23 -0300

    exercicio 3 ano

---
+++ aula1/exerc01.js
@@ -0,0 +1,3 @@
+const pessoa = (nome, idade) => { if (idade > 65) { console.log(`${nome} com ${idade} anos tem direito de aposentadoria.`) } else { console.log(`${nome} com ${idade} anos NÃO tem direito a aposentadoria.`) } }
+
+pessoa("Evandres", 80)
