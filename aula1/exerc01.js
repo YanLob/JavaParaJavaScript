ace97d8b115416d0f50d1465877d0a65a4298e45
YanLob <yanlobate@gmail.com>
2023-08-12 19:20:32 -0300

    abababu

--- aula1/exerc01.js
+++ aula1/exerc01.js
@@ -1,3 +1,10 @@
-const pessoa = (nome, idade) => { if (idade > 65) { console.log(`${nome} com ${idade} anos tem direito de aposentadoria.`) } else { console.log(`${nome} com ${idade} anos NÃO tem direito a aposentadoria.`) } }
+const pessoa = (nome, idade) => { 
+    if (idade > 65) { 
+        console.log(`${nome} com ${idade} anos tem direito de aposentadoria.`) 
+    }
+    else { 
+        console.log(`${nome} com ${idade} anos NÃO tem direito a aposentadoria.`) 
+    } 
+}
 
 pessoa("Evandres", 80)
